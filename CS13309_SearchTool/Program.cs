15ff9e61a80e7afea7cc234bdc9bc3ec1d8ea67f
aldocesarmg <aldocesarmg@gmail.com>
2022-05-09 20:26:45 -0500

    Verificando el funcionamiento de los parametros por consola

---
+++ CS13309_SearchTool/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace CS13309_SearchTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 1)
+            {
+                if (args[0].Equals("retrieve"))
+                {
+                    for (int i = 1; i < args.Length; i++)
+                    {
+                        String palabraBuscar = args[i].ToLower();
+                        Console.WriteLine(args[i]);
+                    }
+                }
+            }
+        }
+    }
+}
